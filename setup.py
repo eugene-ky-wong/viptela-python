ac7df5ffd6d9e5db65c9a64fd7dff857f0e9be53
ops-eugwon <ops-eugwon@tech.cpl.sg>
2018-07-10 14:33:47 +0800

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="viptela_python",
-    version="0.1.4",
+    version="0.1.5",
     author="Eugene KY Wong",
     author_email="morphyme@gmail.com",
     description="This is the Cisco Viptela Python SDK",
